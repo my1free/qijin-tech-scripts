f47bb353bbcc74db7415e24453beb2218e3e7b9a
michealyang <yangshangqiang@inke.cn>
2020-09-30 14:34:37 +0800

    update

--- shell/main.sh
+++ shell/main.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-source shellkit/config.sh
-import files.sh
-import base.sh
+source all.sh
 
-#lsdir .
-array=("test" "a" "bbb")
-if  array_contains $array "a" ;then
-    echo "in"
-else
-    echo "not in"
-fi
-
+lsdir .
